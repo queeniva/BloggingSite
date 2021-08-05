0fe0752a7dd2814a70cdb3665bf52e10c4375a00
Queeniva <ngulefacbenazea@gmail.com>
2021-08-05 09:03:33 +0100

    added a backend to the blog app

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 var _ = require('lodash');
+const mongoose = require("mongoose");
 
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -14,15 +15,26 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-const Posts = [];
+
+mongoose.connect('mongodb://localhost:27017/blogDB', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const postSchema = {
+  title:String,
+  content:String
+};
+
+const Post = mongoose.model("Post",postSchema)
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get('/', function (req, res) {
-  res.render("home", { homeContent: homeStartingContent, homePost: Posts });
-  // console.log(Posts);
+  Post.find({}, function(err, post){
+    res.render("home", {homeContent: homeStartingContent, homePost: post
+      });
+ 
+  })
 });
 
 app.get('/about', (req, res) => {
@@ -41,35 +53,28 @@ app.get("/post", function (req, res) {
   res.render('post', { homePost: Posts })
 });
 
-app.get("/post/:news", function (req, res) {
-  const parameter = _.lowerCase(req.params.news)
-
-  for (let i = 0; i < Posts.length; i++) {
-    const postTitle = _.lowerCase(Posts[i].title)
-    if (parameter == postTitle) {
-      res.render('post', { homePost: Posts[i] })
-    }
-  }
-});
+app.get("/posts/:postId", function(req, res){
+  const requestedPostId = req.params.postId;
+    Post.findOne({_id: requestedPostId}, function(err, post){
+      res.render("post", {
+        title: post.title,
+        content: post.content
+      });
+    });
+  
+  });
 
 app.post("/compose", function (req, res) {
-  const post = { title: req.body.title, bodypost: req.body.bodypost };
-  Posts.push(post)
-  res.redirect('/');
+  const post = new Post({
+    title:req.body.title,
+    content:req.body.bodypost
+  })
+  post.save(function(err){
+    if(!err){
+      res.redirect('/');
+    }
+  });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
